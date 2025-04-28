619ae7d806672cc2d985225b655573795ab0165d
9410o8279 <9410o8279@gmail.com>
2025-04-28 15:07:59 +0530

    Custom content for App.js

--- src/App.js
+++ src/App.js
@@ -23,3 +23,33 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Text to Video App!</h1>
+        <p>This is my custom React app content.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Text to Video App!</h1>
+        <p>This is my custom React app content.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
