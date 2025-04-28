844ea5152713971ec9bc0bbee776b32304310e56
9410o8279 <9410o8279@gmail.com>
2025-04-28 15:23:00 +0530

    Reset App.js to original code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
 import React from 'react';
 import './App.css';
 
@@ -30,23 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Text to Video App!</h1>
-        <p>This is my custom React app content.</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Text to Video App!</h1>
-        <p>This is my custom React app content.</p>
+        <h1>Welcome to React</h1>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
       </header>
     </div>
   );
